9e4c383a8aafb7af0809a5b6e0240e765dfa8198
Maniusdezeb <daanverbeek15@gmail.com>
2023-02-14 11:07:32 +0100

    WIP

--- GroeneVingers/src/app/app-routing.module.ts
+++ GroeneVingers/src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent
-  }
-
+  { path: '**', component: HomePageComponent }
 ];
 
-@NgModule({
+@NgModule({ //
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- GroeneVingers/src/app/app.module.ts
+++ GroeneVingers/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomePageComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
